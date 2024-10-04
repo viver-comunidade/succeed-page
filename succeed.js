54c1485644034a37eda58d468f5221a9d3da43ec
valterjunior <featcode@mercadocar.com.br>
2024-10-04 20:05:30 -0300

    youtube div style fixed.

--- succeed.js
+++ succeed.js
@@ -4,27 +4,27 @@ const copyPixButton = document.getElementById("copy-pix-button");
 const pixCodeInput = document.getElementById("pix-code");
 const pixTooltip = document.getElementById("pix-tooltip");
 
-
+document.body.style.overflow = "hidden"; // Disables background scrolling
 
 copyPixButton.addEventListener("click", () => {
-    pixCodeInput.select();
-    pixCodeInput.setSelectionRange(0, 99999);
-  
-    navigator.clipboard
-      .writeText(pixCodeInput.value)
-      .then(() => {
-        pixTooltip.style.display = "block";
-        pixTooltip.classList.add("show-tooltip");
-  
-        // Hide the tooltip after 2 seconds
+  pixCodeInput.select();
+  pixCodeInput.setSelectionRange(0, 99999);
+
+  navigator.clipboard
+    .writeText(pixCodeInput.value)
+    .then(() => {
+      pixTooltip.style.display = "block";
+      pixTooltip.classList.add("show-tooltip");
+
+      // Hide the tooltip after 2 seconds
+      setTimeout(() => {
+        pixTooltip.classList.remove("show-tooltip");
         setTimeout(() => {
-          pixTooltip.classList.remove("show-tooltip");
-          setTimeout(() => {
-            pixTooltip.style.display = "none";
-          }, 300); // wait for transition to end before hiding
-        }, 2000);
-      })
-      .catch((error) => {
-        console.error("Erro ao copiar o código PIX:", error);
-      });
-  });
+          pixTooltip.style.display = "none";
+        }, 300); // wait for transition to end before hiding
+      }, 2000);
+    })
+    .catch((error) => {
+      console.error("Erro ao copiar o código PIX:", error);
+    });
+});
