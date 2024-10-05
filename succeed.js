430aa57b8bc3e877af16209733a323edc4cc8859
valterjunior <featcode@mercadocar.com.br>
2024-10-05 14:57:27 -0300

    html, js file and css fixed.

--- succeed.js
+++ succeed.js
@@ -1,34 +1,36 @@
-const successElement = document.getElementById("success-message");
 const backButton = document.getElementById("back-button");
 const copyPixButton = document.getElementById("copy-pix-button");
 const pixCodeInput = document.getElementById("pix-code");
 const pixTooltip = document.getElementById("pix-tooltip");
 
-document.body.style.overflow = "hidden"; // Disables background scrolling
-
 backButton.addEventListener("click", () => {
   window.location.href = "https://viver-comunidade.github.io/inscricao-retiro/";
 });
 
-copyPixButton.addEventListener("click", () => {
-  pixCodeInput.select();
-  pixCodeInput.setSelectionRange(0, 99999);
+// Handle PIX code copy with tooltip display
+copyPixButton.addEventListener(
+  "click",
+  () => {
+    pixCodeInput.select();
+    pixCodeInput.setSelectionRange(0, 99999); // For mobile devices
 
-  navigator.clipboard
-    .writeText(pixCodeInput.value)
-    .then(() => {
-      pixTooltip.style.display = "block";
-      pixTooltip.classList.add("show-tooltip");
+    navigator.clipboard
+      .writeText(pixCodeInput.value)
+      .then(() => {
+        pixTooltip.style.display = "block";
+        pixTooltip.classList.add("show-tooltip");
 
-      // Hide the tooltip after 2 seconds
-      setTimeout(() => {
-        pixTooltip.classList.remove("show-tooltip");
+        // Hide the tooltip after 2 seconds
         setTimeout(() => {
-          pixTooltip.style.display = "none";
-        }, 300); // wait for transition to end before hiding
-      }, 2000);
-    })
-    .catch((error) => {
-      console.error("Erro ao copiar o código PIX:", error);
-    });
-});
+          pixTooltip.classList.remove("show-tooltip");
+          setTimeout(() => {
+            pixTooltip.style.display = "none";
+          }, 300); // wait for transition to end before hiding
+        }, 2000);
+      })
+      .catch((error) => {
+        console.error("Erro ao copiar o código PIX:", error);
+      });
+  },
+  { passive: true }
+); // Passive listener added to avoid touch issues
